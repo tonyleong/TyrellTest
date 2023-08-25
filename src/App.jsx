1ac4cca8854e837f7ee6bb30058f50041afaf740
Tony <59461665+tonyleong@users.noreply.github.com>
2023-08-26 00:40:21 +0800

    card distribute function

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function distributeCards(numPeople) {
+  if (!numPeople || isNaN(numPeople) || numPeople < 0) {
+    return ["Input value does not exist or value is invalid"];
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const suits = ['S', 'H', 'D', 'C'];
+  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'];
+  const cards = [];
+
+  suits.forEach(suit => {
+    ranks.forEach(rank => {
+      cards.push(`${suit}-${rank}`);
+    });
+  });
+
+  // Shuffle the cards
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+
+  const peopleHands = Array.from({ length: numPeople }, () => []);
+
+  cards.forEach((card, index) => {
+    const personIndex = index % numPeople;
+    peopleHands[personIndex].push(card);
+  });
+
+  return peopleHands.map(personHand => personHand.join(','));
 }
 
-export default App
+function App() {
+  try {
+    const [numPeople, setNumPeople] = useState('');
+    const [distributedCards, setDistributedCards] = useState([]);
+
+    const handleDistribution = () => {
+      const distributedHands = distributeCards(parseInt(numPeople));
+      setDistributedCards(distributedHands);
+    };
+
+    const handleKeyUp = (e) => {
+      if (e.key === 'Enter') {
+        handleDistribution()
+      }
+    }
+
+    return (
+      <div className='container'>
+        <h1>Card Distribution</h1>
+        <label>
+          Number of People:
+          <input
+            type="number"
+            value={numPeople}
+            onChange={e => setNumPeople(e.target.value)}
+            onKeyUp={handleKeyUp}
+          />
+        </label>
+        <button onClick={handleDistribution}>Distribute Cards</button>
+        <div>
+          {distributedCards.length > 0 &&
+            distributedCards.map((hand, index) => (
+              <p key={index}>{hand}</p>
+            ))}
+        </div>
+      </div>
+    );
+  } catch (error) {
+    return <div className='container'><h1>Irregularity occurred</h1></div>
+  }
+}
+
+export default App;
